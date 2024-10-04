c3b6e2369030bee677f3149a99b7d256ed2b06b9
Zach Nussbaum <zanussbaum@gmail.com>
2024-10-03 23:51:45 -0400

    feat: ln op

---
+++ src/ops/ln.ts
@@ -0,0 +1,24 @@
+import { Tensor } from "../tensor/tensor.js";
+import { UnaryOp } from "../autograd/function.js";
+
+export class Ln extends UnaryOp {
+  protected readonly shaderPath: string = "/src/shaders/ln.wgsl";
+  async backward(grad_output: Tensor): Promise<Tensor[]> {
+    if (!this.context) {
+      throw new Error("Context is null; did you already call ln.backward?");
+    }
+    const [input] = this.context.saved_tensors;
+
+    this.context = null;
+
+    // The gradient of ln(x) is 1 / x
+    const inverseArray = new Float32Array(input.data.length);
+    for (let i = 0; i < input.data.length; i++) {
+      inverseArray[i] = 1 / input.data[i];
+    }
+
+    const grad_inverse = new Tensor(inverseArray, input.shape, input.requires_grad);
+
+    return [grad_inverse];
+  }
+}
